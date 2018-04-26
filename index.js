1c935aaf95f0a355b70931b0403e520df7bcc844
Mike Botsko <botsko@gmail.com>
2018-04-26 07:31:42 -0700

    Initial import

---
+++ index.js
@@ -0,0 +1,36 @@
+const through2 = require('through2');
+const uglify = require('uglify-js');
+
+module.exports = function(opts) {
+    return () => {
+        return through2.obj(function(file, encoding, cb) {
+            if (file.isNull()) {
+                // nothing to do
+                return cb(null, file);
+            }
+
+            try {
+                const result = uglify.minify(file.contents.toString());
+
+                if (result.error) {
+                    this.emit('spearhook:error', {
+                        error: result.error,
+                        file
+                    });
+                }
+
+                if (result.code) {
+                    file.contents = new Buffer(result.code);
+                } else {
+                    const err = new Error('Uglify failed to handle file.');
+
+                    this.emit('spearhook:error', { err, file });
+                }
+            } catch (err) {
+                this.emit('spearhook:error', { err, file });
+            }
+
+            cb(null, file);
+        });
+    }
+};
