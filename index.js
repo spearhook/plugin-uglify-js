a8cc9f5eda0ebd13bdd70d1ee9f683354d0339b8
Mike Botsko <botsko@gmail.com>
2019-02-14 13:31:59 -0800

    Updates dependencies, fixes function format

--- index.js
+++ index.js
@@ -2,35 +2,33 @@ const through2 = require('through2');
 const uglify = require('uglify-js');
 
 module.exports = function(opts) {
-    return () => {
-        return through2.obj(function(file, encoding, cb) {
-            if (file.isNull()) {
-                // nothing to do
-                return cb(null, file);
+    return () => through2.obj(function(file, encoding, cb) {
+        if (file.isNull()) {
+            // nothing to do
+            return cb(null, file);
+        }
+
+        try {
+            const result = uglify.minify(file.contents.toString());
+
+            if (result.error) {
+                this.emit('spearhook:error', {
+                    error: result.error,
+                    file
+                });
             }
 
-            try {
-                const result = uglify.minify(file.contents.toString());
+            if (result.code) {
+                file.contents = Buffer.from(result.code);
+            } else {
+                const err = new Error('Uglify failed to handle file.');
 
-                if (result.error) {
-                    this.emit('spearhook:error', {
-                        error: result.error,
-                        file
-                    });
-                }
-
-                if (result.code) {
-                    file.contents = Buffer.from(result.code);
-                } else {
-                    const err = new Error('Uglify failed to handle file.');
-
-                    this.emit('spearhook:error', { err, file });
-                }
-            } catch (err) {
                 this.emit('spearhook:error', { err, file });
             }
+        } catch (err) {
+            this.emit('spearhook:error', { err, file });
+        }
 
-            cb(null, file);
-        });
-    }
+        cb(null, file);
+    })
 };
