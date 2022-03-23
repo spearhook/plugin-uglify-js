5d8e11c8e4e3df785170b00a16f936014d77f4ad
Mike Botsko <botsko@gmail.com>
2022-03-23 12:12:51 -0700

    Converts to es6 module

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const through2 = require('through2');
-const uglify = require('uglify-js');
+import through2 from 'through2';
+import uglify from 'uglify-js';
 
-module.exports = function(opts) {
+export default function(opts) {
     return () => through2.obj(function(file, encoding, cb) {
         if (file.isNull()) {
             // nothing to do
